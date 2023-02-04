f86446d95773658d4de769aebf40c2f3c0e6ee25
AbdihakiimAhmed <doolyare5354@gmail.com>
2023-02-04 23:04:22 +0300

    first commit

---
+++ lib/utils/colorConst.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color baseColor = Color.fromARGB(255, 231, 185, 35);
+const Color baseColorLight = Color.fromARGB(255, 21, 228, 100);
+const Color redColorLight = Colors.white;
